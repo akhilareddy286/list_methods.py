575fd42fe4ba9272b39fa2cc25d10f577c601620
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-04-06 22:34:57 +0530

    Add files via upload

---
+++ day_19.py
@@ -0,0 +1,101 @@
+
+# a=[]
+# print(a,type(a))
+
+# p=["Ameerpet",66]
+# print(p,type(p))
+# print(p[0],type(p[0]))
+# print(p[1],type(p[1]))
+
+# a=None
+# print(a,type(a))
+
+# a=[1,5.6,"akhila",None,True]
+# print(a,type(a))
+# print(a[0],type(a[0]))
+# print(a[1],type(a[1]))
+# print(a[2],type(a[2]))
+# print(a[3],type(a[3]))
+# print(a[4],type(a[4]))
+# print(a[5],type(a[5])) #Indexerror:list index out of range
+
+# a=[5,7.8,"akhila"]
+# a[0]=68
+# a[2]="AK"
+# print(a)
+
+# b=[2,4.5,"akhi", [5,10] ]
+# b[0]=8 
+# b=[3][0]=60
+# print(b,type(b))
+# print(b[0],type(b[0]))
+# print(b[1],type(b[1]))
+# print(b[2],type(b[2]))
+# print(b[3],type(b[3]))
+
+# a=[1,2.3,"akhila",[5,8]]
+# a.append('reddy')
+# print(a)
+
+# a=[1,2.3,"akhila",[6,8]]
+# a.clear()
+# print(a)
+
+# a=[1,2,3,4]
+# print(a)
+# b=a.copy()
+# print(b)
+
+# a=[2,3,"akhila",6,'']
+# print(a.count(' '))
+# print(a.count(3))
+# print(a.count("akhila"))
+# print(a.count(6))
+
+# a=[1,2,3,4]
+# b=[9,8,7,6]
+# a.extend(b)
+# print(a)
+# b.extend(a)
+# print(b)
+
+# a=[1,3,5,9,11,13]
+# a.insert(3,15)
+# a.insert(2,17)
+# a.insert(-2,19)
+# a.insert(-4,16)
+# print(a)
+
+# c=[20,40,60,80]
+# c.pop()
+# print(c)
+# c.pop(1)
+# c.pop(2)
+# print(c)
+
+# a=[1,2,"akhila",6,9]
+# a.remove(6)
+# a.index(2)
+# print(a)
+
+# s=[1,2,3,4,5]
+# print(s[ ::-1])
+# s.reverse()
+# print(s)
+
+# a=[6,8,3,2,9]
+# a.sort()
+# a.sort(reverse=False)
+# print(a)
+# a.sort(reverse=True)
+# print(a)
+
+# s=[2.3,4.5,6.7,1.2,3.4]
+# s.sort(reverse=True)
+# print(s)
+
+# a=['AK','HI','LA','AZ','RT','as','yt','df']
+# a.sort(reverse=True)
+# print(a)
+
+# print(dir(list))
